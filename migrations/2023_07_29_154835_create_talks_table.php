02f6ade97bc41260ad771e288a41f8910f0bc4c2
Wouter de Jong <wouter@wouterj.nl>
2023-07-29 15:55:46 +0200

    PATCH: Complete Talk migration

--- migrations/2023_07_29_154835_create_talks_table.php
+++ migrations/2023_07_29_154835_create_talks_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('talks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->string('author');
+            $table->boolean('accepted');
+            $table->text('abstract');
+            $table->text('proposal_notes');
         });
     }
 
