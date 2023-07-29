de87087618db4361962f4dc0c93e97048ab74c71
Wouter de Jong <wouter@wouterj.nl>
2023-07-29 16:05:51 +0200

    PATCH: Complete Talk factory

--- src/Factory/TalkFactory.php
+++ src/Factory/TalkFactory.php
@@ -2,6 +2,7 @@
 
 namespace App\Factory;
 
+use Faker\Factory as FakerFactory;
 use WouterJ\EloquentBundle\Factory\Factory;
 
 /**
@@ -16,8 +17,19 @@ class TalkFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = FakerFactory::create();
+
         return [
-            //
+            'title' => trim($faker->sentence(3), '.'),
+            'author' => $faker->name(),
+            'accepted' => false,
+            'abstract' => $faker->paragraph(),
+            'proposal_notes' => $faker->randomDigit() > 5 ? $faker->paragraph() : '',
         ];
+    }
+
+    public function accepted(): Factory
+    {
+        return $this->state(fn (array $attributes): array => ['accepted' => true]);
     }
 }
