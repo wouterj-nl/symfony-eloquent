9f29bfb5b9c887f5616e5a92bc4859dd217e8bdb
Wouter de Jong <wouter@wouterj.nl>
2023-07-29 16:06:22 +0200

    CMD: bin/console make:seeder -n Talk

---
+++ src/Seed/TalkSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Seed;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use WouterJ\EloquentBundle\Seeder;
+
+class TalkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
