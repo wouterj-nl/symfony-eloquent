b6b5a25a9d1c05e6c33f5b29cf80f6bd45a7ad59
Wouter de Jong <wouter@wouterj.nl>
2023-07-29 15:57:32 +0200

    CMD: bin/console make:factory -n Talk

---
+++ src/Factory/TalkFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Factory;
+
+use WouterJ\EloquentBundle\Factory\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Model\Talk>
+ */
+class TalkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
