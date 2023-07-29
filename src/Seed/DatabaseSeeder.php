05db548234c3a3f4a7565161304d20e6b207d0e8
Wouter de Jong <wouter@wouterj.nl>
2023-07-29 16:11:48 +0200

    CMD: bin/console make:seeder -n Database

---
+++ src/Seed/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Seed;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use WouterJ\EloquentBundle\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
